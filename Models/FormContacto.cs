a715076306013bcd3f162f3a7d7fca7b1de7b3fd
caballerocsc <cesarsolau@hotmail.com>
2019-10-25 00:26:27 -0500

    cambios generales

--- Models/FormContacto.cs
+++ Models/FormContacto.cs
@@ -5,29 +5,29 @@ namespace taller_netcore.Models
 {
     public class FormContacto
     {
-        [Required(ErrorMessage = "Este campo es requerido.")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Este campo debe tener entre 3 y 50 caracteres.")]
+        [Required(ErrorMessage = "Campo Obligatorio.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Este campo debe tener entre 3 y 50 caracteres.")]
         public string Nombres { get; set; }
         
-        [Required(ErrorMessage = "Este campo es requerido.")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Este campo debe tener entre 3 y 50 caracteres.")]
+        [Required(ErrorMessage = "Campo Obligatorio.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Este campo debe tener entre 3 y 50 caracteres.")]
         public string Apellidos { get; set; }
         
-        [Required(ErrorMessage = "Este campo es requerido.")]
-        [StringLength(100)]
+        [Required(ErrorMessage = "Campo Obligatorio.")]
+        [StringLength(64)]
         [Display(Name = "Dirección")]
         public string Direccion { get; set; }
         
-        [Required(ErrorMessage = "Este campo es requerido.")]
+        [Required(ErrorMessage = "Campo Obligatorio.")]
         [EmailAddress]
         public string Correo { get; set; }
 
-        [Required(ErrorMessage = "Este campo es requerido.")]
+        [Required(ErrorMessage = "Campo Obligatorio.")]
         [StringLength(15, MinimumLength = 3, ErrorMessage = "Este campo debe tener entre 3 y 15 caracteres.")]
         [Display(Name = "Teléfono")]
         public string Telefono { get; set; }
 
-        [Required(ErrorMessage = "Este campo es requerido.")]
+        [Required(ErrorMessage = "Campo Obligatorio.")]
         public string Comentario { get; set; }
     }
 }
